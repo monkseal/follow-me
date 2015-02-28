24dc67f3f4472aa3b777b058c9eab0cbce1fefd6
Kevin English <kenglish@monksealsoftware.com>
2015-02-27 17:26:35 -0800

    Add TwitterStatus class

--- mytweets.js
+++ mytweets.js
@@ -1,18 +1,16 @@
-// https://www.npmjs.com/package/read-yaml
-// Very thin wrapper around js-yaml for directly reading in YAML files.
+"use strict"
+import TwitterStatus from './lib/twitter-status';
+
+
+//www.npmjs.com/package/read-yaml
+// // Very thin wrapper around js-yaml for directly reading in YAML files.
 var ReadYaml = require('read-yaml');
-// https://www.npmjs.com/package/twitter
-var Twitter = require('twitter');
+// // https://www.npmjs.com/package/twitter
+var Twit = require('twit');
+
+console.log(TwitterStatus)
 
 var config = ReadYaml.sync('twitter.yml');
-var client = new Twitter(config);
-
-var params = {screen_name: 'videodrome_pod'};
-client.get('statuses/user_timeline', params, function(error, tweets, response){
-  if (!error) {
-    tweets.forEach(function(tweet, _i) {
-      console.log("TWEET " + _i);
-      console.log(tweet.text);
-    });
-  }
-});
+var client = new Twit(config);
+var twitterStatus = new TwitterStatus(client);
+twitterStatus.showStatusFor('videodrome_pod')
