6e3b8148ebb3fb62295f1e4b2634970aa8ea47e8
Kevin English <kenglish@monksealsoftware.com>
2015-02-28 16:13:24 -0800

    Update TwitterStatus test to remove mock of Twit module

--- __tests__/twitter-status-test.js
+++ __tests__/twitter-status-test.js
@@ -1,11 +1,7 @@
 jest.dontMock('../lib/twitter-status');
 
 import TwitterStatus from '../lib/twitter-status';
-jest.mock('Twit');
-
-var ReadYaml = require('read-yaml');
 var Twit = require('twit');
-var config = ReadYaml.sync('twitter.yml');
 var client = new Twit({});
 
 describe('TwitterStatus', function() {
@@ -13,10 +9,10 @@ describe('TwitterStatus', function() {
   describe('#showStatusFor', function() {
     it('calls with correct params', function() {
       var twitterStatus = new TwitterStatus(client);
-      twitterStatus.showStatusFor('videodrom_pod' );
+      twitterStatus.showStatusFor('videodrome_pod' );
       expect(client.get.mock.calls.length).toEqual(1);
       expect(client.get.mock.calls[0][0]).toEqual('statuses/user_timeline')
-      expect(client.get.mock.calls[0][1]).toEqual({screen_name: 'videodrom_pod'})
+      expect(client.get.mock.calls[0][1]).toEqual({screen_name: 'videodrome_pod'})
     });
   });
 
